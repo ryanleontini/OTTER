5ebd35c7e8f2124986741fd4a07560bb4ae6e4c2
ryanleontini <79189079+ryanleontini@users.noreply.github.com>
2023-02-22 09:32:23 -0800

    Update OTTER_MCU.sv

--- OTTER_MCU.sv
+++ OTTER_MCU.sv
@@ -21,17 +21,38 @@ module OTTER_MCU(
     );
     
     // PC
-    logic [31:0] PC_4;
-    logic [31:0] PC_OUT;
-    
-    // 6:1 Mux entering the PC.
-    logic [31:0] PC_DIN;
+    logic [31:0] jalr, branch, jal;
+    logic MTVEC, MEPC, reset, PCWrite;
+    logic [2:0] pcSource;
+    logic [31:0] PC_4, PC_OUT, PC_DIN;
+    // MEMORY
+    logic [1:0] MEM_SIZE;
+    logic MEM_SIGN, memRDEN1, memRDEN2, memWE2;
+    logic [31:0] ir, DOUT2;
+    logic [13:0] PC_15_2;
+    // REG_FILE
+    logic [31:0] wd, rs1, rs2;
+    logic [1:0] rf_wr_sel;
+    logic csr_RD, regWrite;
+    logic [4:0] adr1, adr2, wa;
+    // IMMED GEN
+    logic [24:0] INSTRUCT;
+    logic [31:0] U_TYPE, I_TYPE, S_TYPE, J_TYPE, B_TYPE;
+    // ALU
+    logic [1:0] alu_srcA;
+    logic [2:0] alu_srcB;
+    logic [3:0] alu_fun;
+    logic [31:0] srcA, srcB, result;
+    // BRANCH COND GEN
+    logic br_eq, br_lt, br_ltu;
+    // CU_FSM
+    logic csr_WE, int_taken, mret_exec;
     
     // SET TO ZERO UNTIL IMPLEMENTED.
-    logic MTVEC, MEPC;
     assign MTVEC = 0;
     assign MEPC = 0;
     
+    // 6:1 Mux entering the PC.
     always_comb begin
         case (pcSource)
             3'b000: PC_DIN <= PC_4;
@@ -51,10 +72,9 @@ module OTTER_MCU(
     
     // END OF PC.
     
-    // MEMORY
-    logic [31:0] ir;
-    logic [13:0] PC_15_2;
-    
+    // MEMORY   
+    assign MEM_SIZE = ir[13:12];
+    assign MEM_SIGN = ir[14];
     assign PC_15_2 = {PC_OUT[15:2]};
     
     // memory module.
@@ -66,13 +86,11 @@ module OTTER_MCU(
     // END OF MEMORY.
     
     // REG_FILE
-    // 4:1 Mux entering the REG_FILE.
-    logic [31:0] wd;
     
     // SET ZERO UNTIL CSR IS IMPLEMENTED
-    logic csr_RD;
     assign csr_RD = 0;
     
+    // 4:1 Mux entering the REG_FILE.
     always_comb begin
         case (rf_wr_sel)
             2'b00: wd = PC_4;
@@ -83,10 +101,11 @@ module OTTER_MCU(
     end
     
     // REG_FILE input logic.
-    logic [4:0] adr1, adr2, wa;
     assign adr1 = ir[19:15];
     assign adr2 = ir[24:20];
     assign wa = ir[11:7];
+    
+    // REG_FILE output logic.
     
     // reg_file module.
     REG_FILE reg_file(.RF_ADR1(adr1), .RF_ADR2(adr2), .RF_EN(regWrite), .RF_WA(wa), .RF_WD(wd), 
@@ -95,8 +114,8 @@ module OTTER_MCU(
     // END OF REG_FILE.
     
     // IMMED GEN
-    logic [24:0] INSTRUCT;
     assign INSTRUCT = ir[31:7];
+
     
     // immed_gen module.
     IMMED_GEN immed_gen(.INSTRUCT(INSTRUCT), .U_TYPE(U_TYPE), .I_TYPE(I_TYPE), 
@@ -106,7 +125,7 @@ module OTTER_MCU(
     
     // BRANCH ADDR GEN
     // branch_addr_gen module.
-    BRANCH_ADDR_GEN branch_addr_gen(.PC(PC), .JType(J_TYPE), .BType(B_TYPE), .IType(I_TYPE), 
+    BRANCH_ADDR_GEN branch_addr_gen(.PC(PC_OUT), .JType(J_TYPE), .BType(B_TYPE), .IType(I_TYPE), 
     .rs1(rs1), .jal(jal), .branch(branch), .jalr(jalr));
     
     // END OF BRANCH ADDR GEN.
@@ -118,10 +137,11 @@ module OTTER_MCU(
     // END OF BRANCH COND GEN.
     
     // ALU
+
     // 3:1 Mux going into srcA.
     logic [31:0] srcA;
     always_comb begin
-        case (srcA)
+        case (alu_srcA)
             2'b00: srcA = rs1;
             2'b01: srcA = U_TYPE;
             2'b10: srcA = ~rs1;
@@ -132,11 +152,11 @@ module OTTER_MCU(
     // 5:1 Mux going into srcB.
     logic [31:0] srcB;
     always_comb begin
-        case (srcB)
+        case (alu_srcB)
             3'b000: srcB = rs2;
             3'b001: srcB = I_TYPE;
             3'b010: srcB = S_TYPE;
-            3'b011: srcB = PC;
+            3'b011: srcB = PC_OUT;
             3'b100: srcB = csr_RD;
             default: srcB = 32'h000BDEAD;
         endcase
@@ -147,22 +167,24 @@ module OTTER_MCU(
     
     // END OF ALU.
     
-    // cu_fsm module.
+    // CU_FSM
     
-    logic csr_WE, int_taken, mret_exec;
     assign csr_WE = 0;
     assign int_taken = 0;
     assign mret_exec = 0;
     
+    // cu_fsm module.
     CU_FSM cu_fsm(.RST(RST), .ir6_0(ir[6:0]), .ir14_12(ir[14:12]), .CLK(CLK), .PCWrite(PCWrite), .regWrite(regWrite), .memWE2(memWE2), 
         .memRDEN1(memRDEN1), .memRDEN2(memRDEN2), .reset(reset), .csr_WE(csr_WE), .int_taken(int_taken), .mret_exec(mret_exec));
         
     // END OF CU_FSM.
     
-    // cu_dcdr
+    // CU_DCDR
+    
+    // cu_dcdr module.
     
     CU_DCDR cu_dcdr(.ir14_12(ir[14:12]), .ir30(ir[30]), .ir6_0(ir[6:0]), .int_taken(int_taken), .br_eq(br_eq), .br_lt(br_lt), .br_ltu(br_ltu), 
-        .alu_fun(alu_fun), .alu_srcA(srcA), .alu_srcB(srcB), .pcSource(pcSource), .rf_wr_sel(rf_wr_sel)); 
+        .alu_fun(alu_fun), .alu_srcA(alu_srcA), .alu_srcB(alu_srcB), .pcSource(pcSource), .rf_wr_sel(rf_wr_sel)); 
     
-    
+    // END OF CU_DCDR
 endmodule
