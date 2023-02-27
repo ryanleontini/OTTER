130d1006caf2953d04c688424f49843cbccd5383
ryanleontini <79189079+ryanleontini@users.noreply.github.com>
2023-02-27 09:44:22 -0800

    Simplifying CU_DCDR

--- srcs/CU_DCDR.sv
+++ srcs/CU_DCDR.sv
@@ -43,50 +43,7 @@ module CU_DCDR(
             // R-Type
             7'b0110011: begin
                 rf_wr_sel = 2'b11;
-                case(ir14_12)
-                    // add and sub
-                    3'b000: begin
-                        // add
-                        if (ir30 == 1'b0) begin
-                            alu_fun = 4'b0000;
-                        end
-                        // sub
-                        else begin
-                            alu_fun = 4'b1000;
-                        end
-                    end
-                    // or
-                    3'b110: begin
-                        alu_fun = 4'b0110;
-                    end
-                    // and
-                    3'b111: begin
-                        alu_fun = 4'b0111;
-                    end
-                    // xor
-                    3'b100: begin
-                        alu_fun = 4'b0100;
-                    end
-                    // sll
-                    3'b001: begin
-                        alu_fun = 4'b0001;
-                    end
-                    // sra
-                    3'b101: begin
-                        alu_fun = 4'b1101;
-                    end
-                    // slt
-                    3'b010: begin
-                        alu_fun = 4'b0010;
-                    end
-                    // sltu
-                    3'b011: begin
-                        alu_fun = 4'b0011;
-                    end
-                    default: begin
-                        alu_fun = 4'b1010;
-                    end
-                endcase
+                alu_fun = {ir[30], ir[14:12]};
             end
             // I-Type (load)
             7'b0000011:begin
@@ -105,45 +62,14 @@ module CU_DCDR(
             7'b0010011: begin
                 rf_wr_sel = 2'b11;
                 alu_srcB = 3'b001;
-                case(ir14_12)
-                    // addi
-                    3'b000: begin
-                        alu_fun = 4'b0000;
-                    end
-                    // andi
-                    3'b111: begin 
-                        alu_fun = 4'b0111;
-                    end
-                    // ori
-                    3'b110: begin
-                        alu_fun = 4'b0110;
-                    end
-                    // slli
-                    3'b001: begin
-                        alu_fun = 4'b0001;
-                    end
-                    // slti
-                    3'b010: begin
-                        alu_fun = 4'b0010;
-                    end
-                    // sltiu
-                    3'b011: begin
-                        alu_fun = 4'b0011;
-                    end
-                    // srai & srli
-                    3'b101: begin
-                        if(ir30 == 1'b1)
-                            // srai
-                            alu_fun = 4'b1101;
-                        else if (ir30 == 1'b0)
-                            // srli
-                            alu_fun = 4'b0101;
-                    end
-                    // xori
-                    3'b100: begin
-                        alu_fun = 4'b0100;
-                    end
-                endcase
+                if (ir14_12 == 3'b101) begin 
+                    alu_fun = {ir[30], ir[14:12]};
+                end
+                else begin
+                    alu_fun = {1'b0, ir14_12}
+                end
+
+
             end
             // S-Type
             7'b0100011: begin
