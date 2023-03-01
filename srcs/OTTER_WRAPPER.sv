9c2cf1fee6e85aa390d247e36519c05f2e8bda38
ryanleontini <79189079+ryanleontini@users.noreply.github.com>
2023-02-28 22:42:17 -0800

    Update OTTER_WRAPPER.sv

--- srcs/OTTER_WRAPPER.sv
+++ srcs/OTTER_WRAPPER.sv
@@ -17,21 +17,39 @@ module OTTER_WRAPPER(
     output ANODES, CATHODES, LEDS
     )
 
-    logic SWITCHES_INPUTS_MUX_OUT
+    logic SWITCHES_INPUTS_MUX_OUT;
 
     // clock divider
-    logic HALF_CLK
+    logic HALF_CLK;
 
     // switches mux
     logic MUX_SELECT
+    case(MUX_SELECT)
+        // switches address (incorrect)
+        32'h11000000: SWITCHES_INPUTS_MUX_OUT = 32'h11000000;
+        // inputs address (incorrect)
+        32'h11000060: SWITCHES_INPUTS_MUX_OUT = 32'h11000020;
+    endcase
 
     // flip-flop mux
-    logic FF_MUX_IN
+    logic FF_MUX_IN;
+    logic FF_MUX_OUT_1;
+    logic FF_MUX_OUT_2;
+    case(MUX_SELECT)
+        // anodes address (incorrect)
+        32'h11000040: FF_MUX_OUT_1 = IOBUS_WR;
+        // LEDS address (incorrect)
+        32'h1100040: FF_MUX_OUT_2 = IOBUS_WR;
+    endcase
 
     // flip-flops
+    always_ff @ (posedge HALF_CLK) begin
+        
+    end
+
 
     OTTER_MCU otter_mcu(.RST(BTNR), .IOBUS_IN(SWITCHES_INPUTS_MUX_OUT), .CLK(HALF_CLK), 
-    .IOBUS_ADDR(MUX_SELECT), .IOBUS_WR(FF_MUX_IN), .IOBUS_OUT())
+    .IOBUS_ADDR(MUX_SELECT), .IOBUS_WR(FF_MUX_IN), .IOBUS_OUT(IOBUS_OUT));
 
 
 endmodule
